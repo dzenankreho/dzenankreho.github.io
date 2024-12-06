999a51c1e1cff8d6ab738ea29a3cbd845bfb2cdf
Dženan Kreho <92307873+dzenankreho@users.noreply.github.com>
2024-12-06 21:22:13 +0100

    Add responsiveness for mobile

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const modeToggleButton = document.getElementById('mode-toggle');
+const mobileModeToggleButton = document.getElementById('mobile-mode-toggle');
 const html = document.documentElement;
 const menu = document.getElementById("menu");
 const header = document.querySelector("header");
@@ -7,11 +8,9 @@ function closeMobileMenu(){
     menu.classList.remove("active");
 }
 
-
 function toggleMobileMenu(){
 	menu.classList.toggle("active");
 }
-
 
 if (html.classList.contains('dark-mode')) {
     modeToggleButton.checked = true;
@@ -23,45 +22,6 @@ modeToggleButton.addEventListener('click', () => {
     html.classList.toggle('dark-mode');
 });
 
-
-// function debounce(func, wait) {
-//     let timeout;
-//     return function (...args) {
-//       clearTimeout(timeout);
-//       timeout = setTimeout(() => func.apply(this, args), wait);
-//     };
-// }
-
-// window.addEventListener("scroll", debounce(() => {
-//     if (window.matchMedia("(max-width: 1100px)")) {
-//         if (window.scrollY === 0) {
-//             header.style.position = "absolute";
-//           } else {
-//             header.style.position = "sticky";
-//           }
-//     } else {
-//         header.style.position = "sticky";
-//     }
-// }, 10));
-
-
-// function getScrollPercent() {
-//     var h = document.documentElement, 
-//         b = document.body,
-//         st = 'scrollTop',
-//         sh = 'scrollHeight';
-//     return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
-// }
-
-// function changeCss () {
-//     var navElement = document.querySelector("nav");
-//     this.scrollY > 500 ? navElement.style.opacity = .8 : navElement.style.opacity = 1;
-//   }
-
-// window.addEventListener("scroll", () => {
-//     if (getScrollPercent() == 0) {
-
-//     } else {
-
-//     }
-// }, false);
+mobileModeToggleButton.addEventListener('click', () => {
+    html.classList.toggle('dark-mode');
+});
